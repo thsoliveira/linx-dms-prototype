24cf23f08c1f90d181e16c49400e73aa7b6bd3e4
Thais Oliveira <borgesoliveira.thais@gmail.com>
2018-09-13 14:52:40 -0300

    feat(prototype)
    primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
+/* NPM packages */
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { NgxCurrencyModule } from 'ngx-currency';
+import { CURRENCY_MASK_CONFIG } from 'ngx-currency/src/currency-mask.config';
+import { ArchwizardModule } from 'angular-archwizard';
+import { NgbModule, NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
+import { NgSelectModule, NG_SELECT_DEFAULT_CONFIG } from '@ng-select/ng-select';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { LoadingModule } from 'ngx-loading';
+import { ModalModule } from 'ngx-modal';
+import { ChartsModule } from 'ng2-charts';
+import { ToasterModule } from 'angular2-toaster';
+import { CustomCurrencyMaskConfig, NgbDateParserFormatterPtBr } from 'linx-dms-assets';
+
+/* Components */
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,9 +24,24 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ChartsModule,
+    LoadingModule,
+    Ng2SearchPipeModule,
+    NgxCurrencyModule,
+    ArchwizardModule,
+    ModalModule,
+    NgbModule.forRoot(),
+    NgSelectModule,
+    AngularFontAwesomeModule,
+    ToasterModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    { provide: CURRENCY_MASK_CONFIG, useValue: CustomCurrencyMaskConfig },
+    { provide: NgbDateParserFormatter, useClass:  NgbDateParserFormatterPtBr },
+    { provide: NG_SELECT_DEFAULT_CONFIG, useValue: { notFoundText: 'Nenhum resultado encontrado.' } }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

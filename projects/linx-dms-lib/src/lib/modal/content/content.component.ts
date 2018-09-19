2a5b73e55a23b3c5964333d37b022ce7d07743b8
Felipe Cabreira <felipe.cabreira@acad.pucrs.br>
2018-09-19 17:10:54 -0300

    feat(prototype): import bootstrap breadcrumb

--- projects/linx-dms-lib/src/lib/modal/content/content.component.ts
+++ projects/linx-dms-lib/src/lib/modal/content/content.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
-
+  tabOpened: string;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  openTab(tab: string) {
+    this.tabOpened = tab;
   }
 
 }

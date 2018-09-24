56db396c80b712b3a9507b9fe4511cbd98a29b1f
Thais Oliveira <borgesoliveira.thais@gmail.com>
2018-09-24 09:22:57 -0300

    feat(prototype): passada responsabilidade das rotas para lib para fazer a navegação dentro do contexto da pagina principal

--- projects/linx-dms-lib/src/lib/linx-dms-lib-routing.module.ts
+++ projects/linx-dms-lib/src/lib/linx-dms-lib-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LinxDmsLibComponent } from './linx-dms-lib.component';
-
+import { ModalComponent } from './modal/modal.component';
+import { ContentComponent } from './modal/content/content.component';
+import { PagesComponent } from './pages/pages.component';
+import { PageOneComponent } from './pages/page-one/page-one.component';
+import { PageTwoComponent } from './pages/page-two/page-two.component';
+import { PageThreeComponent } from './pages/page-three/page-three.component';
 
 const libRoutes: Routes = [
   {
@@ -10,20 +15,66 @@ const libRoutes: Routes = [
     children: [
       {
         path: 'modal',
-        loadChildren: './modal/modal.module#ModalModule',
-      },
-      {
-        path: 'pages',
-        loadChildren: './pages/pages.module#PagesModule'
+        component: ModalComponent,
+        children: [
+          {
+            path: 'content',
+            // loadChildren: './content/content.module#ContentModule',
+            component: ContentComponent,
+            children: [
+              {
+                path: 'pages',
+                component: PagesComponent,
+                // loadChildren: '../../pages/pages.module#PagesModule',
+                children: [
+                  {
+                    path: 'pageOne',
+                    component: PageOneComponent
+                  },
+                  {
+                    path: 'pageTwo',
+                    component: PageTwoComponent
+                  },
+                  {
+                    path: 'pageThree',
+                    component: PageThreeComponent
+                  },
+                ],
+              }
+            ]
+          },
+        ]
       },
     ]
   },
   {
-    path: '',
-    redirectTo: '/lib',
-    pathMatch: 'full'
-  },
+      path: '',
+      redirectTo: 'lib',
+      pathMatch: 'full'
+  }
 ];
+
+// const libRoutes: Routes = [
+//   {
+//     path: 'lib',
+//     component: LinxDmsLibComponent,
+//     children: [
+//       {
+//         path: 'modal',
+//         loadChildren: './modal/modal.module#ModalModule',
+//       },
+//       {
+//         path: 'pages',
+//         loadChildren: './pages/pages.module#PagesModule'
+//       },
+//     ]
+//   },
+//   {
+//     path: '',
+//     redirectTo: '/lib',
+//     pathMatch: 'full'
+//   },
+// ];
 @NgModule({
   exports: [
     RouterModule
--- projects/linx-dms-lib/src/lib/modal/modal-routing.module.ts
+++ projects/linx-dms-lib/src/lib/modal/modal-routing.module.ts
@@ -1,54 +1,54 @@
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ModalComponent } from './modal.component';
-import { ContentComponent } from './content/content.component';
-import { PagesComponent } from '../pages/pages.component';
-import { PageOneComponent } from '../pages/page-one/page-one.component';
-import { PageTwoComponent } from '../pages/page-two/page-two.component';
-import { PageThreeComponent } from '../pages/page-three/page-three.component';
+// import { ModalComponent } from './modal.component';
+// import { ContentComponent } from './content/content.component';
+// import { PagesComponent } from '../pages/pages.component';
+// import { PageOneComponent } from '../pages/page-one/page-one.component';
+// import { PageTwoComponent } from '../pages/page-two/page-two.component';
+// import { PageThreeComponent } from '../pages/page-three/page-three.component';
 
-const modalRoutes: Routes = [
-    {
-        path: 'modal',
-        component: ModalComponent,
-        children: [
-            {
-                path: 'content',
-                // loadChildren: './content/content.module#ContentModule',
-                component: ContentComponent,
-                children: [
-                    {
-                        path: 'pages',
-                        component: PagesComponent,
-                        // loadChildren: '../../pages/pages.module#PagesModule',
-                        children: [
-                            {
-                                path: 'pageOne',
-                                component: PageOneComponent
-                            },
-                            {
-                                path: 'pageTwo',
-                                component: PageTwoComponent
-                            },
-                            {
-                                path: 'pageThree',
-                                component: PageThreeComponent
-                            },
-                        ],
-                    }
-                ]
-            },
-        ]
-    },
-    /* {
-        path: '',
-        redirectTo: './modal',
-        pathMatch: 'full'
-    } */
-];
+// const modalRoutes: Routes = [
+//     {
+//         path: 'modal',
+//         component: ModalComponent,
+//         children: [
+//             {
+//                 path: 'content',
+//                 // loadChildren: './content/content.module#ContentModule',
+//                 component: ContentComponent,
+//                 children: [
+//                     {
+//                         path: 'pages',
+//                         component: PagesComponent,
+//                         // loadChildren: '../../pages/pages.module#PagesModule',
+//                         children: [
+//                             {
+//                                 path: 'pageOne',
+//                                 component: PageOneComponent
+//                             },
+//                             {
+//                                 path: 'pageTwo',
+//                                 component: PageTwoComponent
+//                             },
+//                             {
+//                                 path: 'pageThree',
+//                                 component: PageThreeComponent
+//                             },
+//                         ],
+//                     }
+//                 ]
+//             },
+//         ]
+//     },
+//     /* {
+//         path: '',
+//         redirectTo: './modal',
+//         pathMatch: 'full'
+//     } */
+// ];
 @NgModule({
     imports: [
-        RouterModule.forChild(modalRoutes)
+        // RouterModule.forChild(modalRoutes)
     ],
     exports: [
         RouterModule
